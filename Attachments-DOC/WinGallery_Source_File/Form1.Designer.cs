235fd0ad5465a0fe9e114d3c6daf8b423f600dac
Drago <94208742+DragoScript@users.noreply.github.com>
2022-06-14 20:36:34 -0400

    Attachment Files for Day 52

---
+++ Attachments-DOC/WinGallery_Source_File/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace WinGallery
+{
+    partial class WinGallery
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_back = new System.Windows.Forms.Button();
+            this.btn_next = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WinGallery.Properties.Resources.img1;
+            this.pictureBox1.Location = new System.Drawing.Point(78, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(648, 396);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(351, 411);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // btn_back
+            // 
+            this.btn_back.Font = new System.Drawing.Font("Arial Black", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_back.Location = new System.Drawing.Point(12, 139);
+            this.btn_back.Name = "btn_back";
+            this.btn_back.Size = new System.Drawing.Size(48, 109);
+            this.btn_back.TabIndex = 2;
+            this.btn_back.Text = "<";
+            this.btn_back.UseVisualStyleBackColor = true;
+            this.btn_back.Click += new System.EventHandler(this.btn_back_Click);
+            // 
+            // btn_next
+            // 
+            this.btn_next.Font = new System.Drawing.Font("Arial Black", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_next.Location = new System.Drawing.Point(740, 139);
+            this.btn_next.Name = "btn_next";
+            this.btn_next.Size = new System.Drawing.Size(48, 109);
+            this.btn_next.TabIndex = 3;
+            this.btn_next.Text = ">";
+            this.btn_next.UseVisualStyleBackColor = true;
+            this.btn_next.Click += new System.EventHandler(this.btn_next_Click);
+            // 
+            // WinGallery
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_next);
+            this.Controls.Add(this.btn_back);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "WinGallery";
+            this.Text = "WinGallery";
+            this.Load += new System.EventHandler(this.WinGallery_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_back;
+        private System.Windows.Forms.Button btn_next;
+    }
+}
+
---
+++ Attachments-DOC/WinGallery_Source_File/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinGallery
+{
+    public partial class WinGallery : Form
+    {
+        public WinGallery()
+        {
+            InitializeComponent();
+        }
+
+        private void WinGallery_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Properties.Resources.img1;
+            label1.Text = "Blue Image";
+        }
+
+        private int counter = 1;
+
+        private void btn_next_Click(object sender, EventArgs e)
+        {
+            counter++;
+            if (counter > 2)
+            {
+                counter = 2;
+            }
+            if (counter == 1)
+            {
+                pictureBox1.Image = Properties.Resources.img1;
+                label1.Text = "Blue Image";
+            }
+            if (counter == 2)
+            {
+                pictureBox1.Image = Properties.Resources.img3;
+                label1.Text = "Green Image";
+            }
+            
+
+        }
+
+        private void btn_back_Click(object sender, EventArgs e)
+        {
+            counter--;
+            if (counter < 0)
+            {
+                counter = 0;
+            }
+            if (counter == 1)
+            {
+                pictureBox1.Image = Properties.Resources.img1;
+                label1.Text = "Blue Image";
+            }
+            if (counter == 0)
+            {
+                pictureBox1.Image = Properties.Resources.img2;
+                label1.Text = "Red Image";
+            }
+        }
+
+
+    }
+}
